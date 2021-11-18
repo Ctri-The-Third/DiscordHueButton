730525d8e2e980ac8cd6237577113de9bf913e09
C'tri Goudie <ctricodigin@ctri.co.uk>
2021-11-18 00:09:51 +0000

    added basic bot outline

--- main.py
+++ main.py
@@ -1,28 +1,71 @@
+import logging
+import threading
+from discord import client
 from gpiozero import PWMLED
 from time import sleep
-
+import discord
 import statusLED
 import gpiozero
-
-def pressed():
-    print("We did it")
-def released():
-    print ("we undid it")
-
-
-             
-led = statusLED.statusLight(18)
-button = gpiozero.DigitalInputDevice(4)
-
-change = 0.01
+import json
+import asyncio
 
 
 
+class ButtonBot(discord.Client):
+    async def on_ready(self):
+        print('Logged in as')
+        print(self.user.name)
+        print(self.user.id)
+        print('------')
+    async def on_message(self,message: discord.message):
+        if message.author.id != self.user.id:
+            await message.reply("hello!", mention_author=True)
+        return 
 
-while True:
-    if button.is_active:
-        led.flash()  
+    async def onButtonPressed(self):
+        print("Bot has triggered button-press!")
+        sleep(10)
+        print("Button press finished")
+        return 
+
+    async def _onButtonPressed():
+        return 
+def GPIOActions(bot: ButtonBot):
+    led = statusLED.statusLight(18)
+    button = gpiozero.DigitalInputDevice(4)
+    oldState = button.is_active
+    firstFoundTimeRemaining = 300
+
+    while firstFoundTimeRemaining > 0:
+        sleep(1)
+        if bot.is_ready():
+            firstFoundTimeRemaining = 0
+    
+    print ("Active")
+    while bot.is_ready():    
+        if button.is_active:
+            led.flash(rate=0.2)  
+        if button.is_active and button.is_active != oldState:
+            asyncio.run(bot.onButtonPressed())
+
+        oldState = button.is_active
+        sleep(.025) 
+
+
+
+if __name__ == "__main__":
+    bot = ButtonBot()
+    token = ""
+    try: 
+        keys = json.load(open("auth.json"))
+        token = keys["token"]      
+        
+    except:
+        logging.error("Bot execution failed, defaulting to bot-less mode")
+    buttonThread = threading.Thread(target=GPIOActions, args=[bot])
+    buttonThread.start()
+    bot.run(token)
+    buttonThread.join()
     
     
-    sleep(.025) 
 
--- statusLED.py
+++ statusLED.py
@@ -40,8 +40,10 @@ class statusLight:
             self._LED.value = self._pulseValue = newValue
             time.sleep(0.01)
             
-
-
+    def swtich_off(self):
+        return 
+    def switch_on(self):
+        return 
     def _bounce(self,newValue):
         if newValue >= 1:
             self._pulsedirection = -1
@@ -68,7 +70,7 @@ class statusLight:
         flashRate = self._flashRate * 2 #e.g. every 0.5 seconds we change, so a full rotation is 1 second
         currentTime = time.time_ns() / 1000 / 1000 / 1000  #get time in seconds
         remainder = currentTime % flashRate  #determine how far through the cycle we are 
-        print("current time in seconds:{} full cycle: {} progress through cycle: {}".format(currentTime,flashRate,currentTime%flashRate))
+        #print("current time in seconds:{} full cycle: {} progress through cycle: {}".format(currentTime,flashRate,currentTime%flashRate))
 
         if remainder - self._flashRate < 0: #if in the first half, light off 
             return 0 
